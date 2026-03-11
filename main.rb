071b410333cb08176436dc7008b6741f2c2198fe
ExMachinery <milkcarrotc@gmail.com>
2026-03-12 01:27:58 +0500

    Add #build_tree method, which build BST recursively

--- main.rb
+++ main.rb
@@ -1,3 +1,4 @@
 require_relative 'tree'
 
-test = Tree.new
+test = Tree.new([1, 2, 3, 4, 5, 6, 7, 8, 9])
+test.pretty_print
--- tree.rb
+++ tree.rb
@@ -4,8 +4,39 @@ class Tree
   attr_accessor :root
   def initialize(array)
     @root = nil
+    build_tree(array.uniq.sort)
+  end
 
-    # some method that will handle array, no need to store it.
+  def build_tree(array)
+    return nil if array.empty?
+    array_mid = (array.size - 1) / 2
+    left_part = array[0...array_mid]
+    right_part = array[array_mid+1..]
+
+    current = nil
+    if !@root
+      @root = Node.new(array[array_mid])
+      current = @root
+    else
+      current = Node.new(array[array_mid])
+    end
+    current.left = build_tree(left_part)
+    current.right = build_tree(right_part)
+    current
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left: true)
+    return unless node
+
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", is_left: false)
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", is_left: true)
   end
 
 end
+
+# Найти центр массива
+# Создать ноду в руте
+# разбить массив на левый и правый от центра
+# Для левого линка найти центр левого массива
+# для правого линка найти центр правого массива
