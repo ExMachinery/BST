c9c29a224f31f67b8b17164f7169e4c1a1f6f985
ExMachinery <milkcarrotc@gmail.com>
2026-03-12 00:37:02 +0500

    Create node (done) and tree (no methods) class. Create main file for testing

---
+++ tree.rb
@@ -0,0 +1,11 @@
+require_relative 'node'
+
+class Tree
+  attr_accessor :root
+  def initialize(array)
+    @root = nil
+
+    # some method that will handle array, no need to store it.
+  end
+
+end
